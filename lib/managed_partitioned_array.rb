56581afc1ae4c4b20f77a978cc9b8ad2ce4fd904
ArityWolf <eigenfruit@gmail.com>
2022-09-13 11:17:20 -0700

    v1.1.2

--- lib/managed_partitioned_array.rb
+++ lib/managed_partitioned_array.rb
@@ -1,4 +1,7 @@
 require_relative 'partitioned_array'
+# VERSION: v1.1.2 (9/13/2022 10:17 am)
+# Prettified
+# Added raise to ManagedPartitionedArray#at_capacity? if @has_capacity == false
 # VERSION: v1.1.1a - got rid of class variables, and things seem to be fully working now
 # VERSION: v1.1.0a - fully functional, but not yet tested--test in the real world
 # VERSION: v1.0.2 - more bug fixes, added a few more tests
@@ -17,36 +20,27 @@ class ManagedPartitionedArray < PartitionedArray
   OFFSET = 1 # This came with the math, but you can just state the PARTITION_AMOUNT in total and not worry about the offset in the end
   DB_SIZE = 20 # Caveat: The DB_SIZE is the total # of partitions, but you subtract it by one since the first partition is 0, in code. that is, it is from 0 to DB_SIZE-1, but DB_SIZE is then the max allocated DB size
   PARTITION_ARCHIVE_ID = 0
-  DEFAULT_PATH = "./DB_TEST" # default fallSback/write to current path
+  DEFAULT_PATH = "./DB_TEST" # default fallback/write to current path
   DEBUGGING = false
   PAUSE_DEBUG = false
-  DB_NAME = 'partitioned_array_slice'  
+  DB_NAME = 'partitioned_array_slice'
   PARTITION_ADDITION_AMOUNT = 5
   MAX_CAPACITY = "data_arr_size" # : :data_arr_size; a keyword to add to the array until its full with no buffer additions
-  HAS_CAPACITY = true # if false, then the max_capacity is ignored
-  @@db_name_with_no_archive = "lol"
-
-  # the 'initial' argument variable should be ignored for now, but I had a feeling that there is an elsif that could be implemented at some point
-  def initialize(partition_addition_amount: PARTITION_ADDITION_AMOUNT, initial: true, max_capacity: MAX_CAPACITY, has_capacity: HAS_CAPACITY, partition_archive_id: PARTITION_ARCHIVE_ID, db_size: DB_SIZE, partition_amount_and_offset: PARTITION_AMOUNT + OFFSET, db_path: DEFAULT_PATH, db_name: DB_NAME) 
-    #if initial #bug: this isn't necessary, but it is a "safeguard(?)" -github copilot 
-      # potential to implement more logic here
-     #  puts "object id: #{self.object_id}"
-      @max_partition_archive_id = 0
-      @original_db_name = strip_archived_db_name(db_name: db_name)
-      # puts "#{db_name} is the original db name"
-      # puts "#{@original_db_name} is the original db name"
-      #exit
-      @max_capacity = max_capacity
-      @latest_id = -1# last entry
-      @partition_archive_id = partition_archive_id
-      @max_capacity = max_capacity_setup!
-      @has_capacity = has_capacity
-      @partition_addition_amount = partition_addition_amount
-      @db_name_with_archive = db_name_with_archive(db_name: @original_db_name, partition_archive_id: @partition_archive_id)    
-      @max_capacity = max_capacity_setup!
-      @dynamically_allocates = false if @max_capacity == "data_arr_size"
-      @dynamically_allocates = true if @max_capacity.is_a? Integer
-      super(partition_addition_amount: @partition_addition_amount, dynamically_allocates: @dynamically_allocates, db_size: db_size, partition_amount_and_offset: partition_amount_and_offset, db_path: db_path, db_name: @db_name_with_archive)
+  HAS_CAPACITY = true # if false, then the max_capacity is ignored and at_capacity? raises if @has_capacity == false
+  def initialize(partition_addition_amount: PARTITION_ADDITION_AMOUNT, max_capacity: MAX_CAPACITY, has_capacity: HAS_CAPACITY, partition_archive_id: PARTITION_ARCHIVE_ID, db_size: DB_SIZE, partition_amount_and_offset: PARTITION_AMOUNT + OFFSET, db_path: DEFAULT_PATH, db_name: DB_NAME) 
+    @max_partition_archive_id = 0
+    @original_db_name = strip_archived_db_name(db_name: db_name)
+    @max_capacity = max_capacity
+    @latest_id = -1 # last entry
+    @partition_archive_id = partition_archive_id
+    @max_capacity = max_capacity_setup!
+    @has_capacity = has_capacity
+    @partition_addition_amount = partition_addition_amount
+    @db_name_with_archive = db_name_with_archive(db_name: @original_db_name, partition_archive_id: @partition_archive_id)    
+    @max_capacity = max_capacity_setup!
+    @dynamically_allocates = false if @max_capacity == "data_arr_size"
+    @dynamically_allocates = true if @max_capacity.is_a? Integer
+    super(partition_addition_amount: @partition_addition_amount, dynamically_allocates: @dynamically_allocates, db_size: db_size, partition_amount_and_offset: partition_amount_and_offset, db_path: db_path, db_name: @db_name_with_archive)
   end
 
   # one keyword available: :data_arr_size
@@ -68,17 +62,14 @@ class ManagedPartitionedArray < PartitionedArray
     return temp
   end
 
-  def save_to_archive!;end
-  
   def load_from_archive!(partition_archive_id:)
     temp = ManagedPartitionedArray.new(max_capacity: @max_capacity, partition_archive_id: partition_archive_id, db_size: @db_size, partition_amount_and_offset: @partition_amount_and_offset, db_path: @db_path, db_name: @db_name_with_archive)
     temp.load_everything_from_files!
-    #p @data_arr
     return temp
-
   end
 
   def at_capacity?
+    raise "There is no limited capacity for this array (@has_capacity == false)" if @has_capacity == false
     return false if @has_capacity == false
     case @max_capacity
       when "data_arr_size"
@@ -134,7 +125,6 @@ class ManagedPartitionedArray < PartitionedArray
     end
   end
 
-
   def save_has_capacity_to_file!
     File.open(File.join("#{@db_path}/#{@db_name}", "has_capacity.json"), "w") do |f|
       f.write(@has_capacity.to_json)
@@ -147,24 +137,17 @@ class ManagedPartitionedArray < PartitionedArray
     end
   end
 
-
-
   def save_last_entry_to_file!
-  #  puts "saving last entry to file"
-  #  puts "latest_id: #{@latest_id}"
-    
-    if @partition_archive_id == 0
-      #temp = db_name_with_archive(db_name: @@db_name_with_no_archive["#{self.object_id}"], partition_archive_id: @partition_archive_id)
+    if @partition_archive_id == 0      
       File.open(File.join("#{@db_path}/#{db_name_with_archive(db_name: @original_db_name)}", 'last_entry.json'), 'w') do |file|
         file.write((@latest_id).to_json)
       end
-    else
-      #temp = db_name_with_archive(db_name: @@db_name_with_no_archive["#{self.object_id}"], partition_archive_id: @partition_archive_id-1)
+    else      
       File.open(File.join("#{@db_path}/#{db_name_with_archive(db_name: @original_db_name)}", 'last_entry.json'), 'w') do |file|
         file.write((@latest_id - 1).to_json)
       end
     end
-  end  
+  end
 
   def load_last_entry_from_file!
     File.open(File.join("#{@db_path}/#{db_name_with_archive(db_name: @original_db_name)}", 'last_entry.json'), 'r') do |file|

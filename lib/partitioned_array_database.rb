8715fb1fa4a4e78c07f763777d536a37d07660af
ArityWolf <eigenfruit@gmail.com>
2023-01-12 15:46:42 -0800

    PAD v0.0.3 - CLEANUP

--- lib/partitioned_array_database.rb
+++ lib/partitioned_array_database.rb
@@ -1,14 +1,14 @@
 require_relative 'file_context_managed_partitioned_array_manager'
+# VERSION v0.0.3 - cleanup
 # VERSION 0.0.2 - Database creation by superfolder
 # VERSION 0.0.1 - Skeleton of the class created
 class PartitionedArrayDatabase
-  attr_accessor :database_folder_name, :manager
+  attr_accessor :database_folder_name, :pad
   FCMPAM_DB_INDEX_NAME = 'FCMPAM_DB_INDEX'
   DB_NAME = 'FCMPAM_DB'
   DATABASE_FOLDER_NAME = './default' # folder name in terms of a full or relative path
   # For a change of database variables, check the file constants in the file_context_managed_partitioned_array_manager.rb library, etc.
   def initialize(database_folder_name: DATABASE_FOLDER_NAME)
-    @database_name = database_name
     @database_folder_name = database_folder_name
     @pad = FileContextManagedPartitionedArrayManager.new(db_path: "#{database_folder_name}/#{DB_NAME}", fcmpa_db_folder_name: "#{database_folder_name}/#{FCMPAM_DB_INDEX_NAME}")
   end

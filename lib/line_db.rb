061e94ff06ad6127ade64758e9dbea26a25c1a7c
ArityWolf <eigenfruit@gmail.com>
2023-01-13 04:39:26 -0800

    line_db.rb + file_methods.rb

---
+++ lib/line_db.rb
@@ -0,0 +1,23 @@
+require_relative "file_methods"
+require_relative "partitioned_array_database"
+class LineDB
+  include FileMethods
+  PARENT_FOLDER = "./database/CGMFS_db"
+  DATABASE_FILE_NAME = "./database/db_list.txt"
+  #require_relative "../lib/partitioned_array/lib/partitioned_array_database"
+  def initialize(parent_folder: PARENT_FOLDER, database_file_name: DATABASE_FILE_NAME)
+    @parent_folder = parent_folder
+    @linelist = load_pad(parent_folder: @parent_folder)
+  end
+
+  def load_pad(parent_folder: @parent_folder || PARENT_FOLDER)
+    db_linelist = read_file_lines(DATABASE_FILE_NAME)
+    #puts db_linelist
+    db_list = {}
+    db_linelist.each do |db_name|
+      db_list[db_name] = PAD.new(database_folder_name: db_name) if !parent_folder
+      db_list[db_name] = PAD.new(database_folder_name: "#{parent_folder}/#{db_name}") if parent_folder
+    end
+    db_list
+  end
+end

26c73f8431fd8c56449565929e6dc6841d80643f
ArityWolf <eigenfruit@gmail.com>
2023-01-13 20:48:39 -0800

    LineDB (lib/line_db.rb) - v1.1.0-release

--- lib/line_db.rb
+++ lib/line_db.rb
@@ -1,25 +1,36 @@
+# rubocop:disable Style/FrozenStringLiteralComment
+# rubocop:disable Metrics/ParameterLists
+# rubocop:disable Style/NegatedIf
+# rubocop:disable Style/GuardClause
+# rubocop:disable Layout/LineLength
+# rubocop:disable Style/MutableConstant
+# rubocop:disable Style/StringLiterals
+
 require_relative "file_methods"
 require_relative "partitioned_array_database"
-# VERSION v1.0.4-release: bug fixes, new features, and tested (partitioned_array/decomposition.rb)
+# LineDB: Line Database - Create a database of PartitionedArrayDatabase objects;
+# which decomposes to FileContextManagedPartitionedArrayManager objects;
+# which decomposes to FileContextManagedPartitionedArray objects;
+# which decomposes to ManagedPartitionedArray objects, which inherits from the PartitionedArray class.
+# VERSION v1.1.0-release: bug fixes, new features, and tested (partitioned_array/decomposition.rb)
 # VERSION v1.0.3-release: bugs fixed and tested
 # VERSION v1.0.2-release: rem_db -> remove_db, bugs fixed and tested
 # VERSION v1.0.1-release: rem_db and add_db methods added
 # VERSION v1.0.0-release
 # VERSION v0.0.1 - Database creation by superfolder
 class LineDB
-  attr_accessor :parent_folder, :database_folder_name, :linelist
-  include FileMethods  
+  attr_accessor :parent_folder, :database_folder_name, :linelist, :database_file_name, :endless_add, :has_capacity, :db_size, :dynamically_allocates, :partition_amount, :traverse_hash
+
+  include FileMethods
+  PAD = PartitionedArrayDatabase
   PARENT_FOLDER = "./database/CGMFS_db"
   DATABASE_FOLDER_NAME = "database"
   DATABASE_FILE_NAME = "./database/db_list.txt"
-  PAD = PartitionedArrayDatabase
-
   ENDLESS_ADD = true
   HAS_CAPACITY = true
   DATABASE_SIZE = 100
   DYNAMICALLY_ALLOCATES = true
   DATABASE_PARTITION_AMOUNT = 20
-
   TRAVERSE_HASH = true
 
   def initialize(traverse_hash: TRAVERSE_HASH, database_partition_amount: DATABASE_PARTITION_AMOUNT, database_file_name: DATABASE_FILE_NAME, endless_add: ENDLESS_ADD, has_capacity: HAS_CAPACITY, db_size: DATABASE_SIZE, dynamically_allocates: DYNAMICALLY_ALLOCATES, parent_folder: PARENT_FOLDER, database_folder_name: DATABASE_FOLDER_NAME)
@@ -58,24 +69,24 @@ class LineDB
   end
 
   # TODO: implement rm_rf for dragonruby on windows and linux, and maybe android
-  def delete_db!(db_name)    
-    FileUtils.rm_rf(db(db_name).database_folder_name)#{@parent_folder}/#{db_name}")
+  def delete_db!(db_name)
+    FileUtils.rm_rf(db(db_name).database_folder_name)
     remove_db!(db_name)
   end
 
   def add_db!(db_name)
-    write_line(db_name) if !check_file_duplicates(db_name)
+    write_line(db_name) unless check_file_duplicates(db_name)
     load_pad_single(db_name)
   end
 
   #### Low level methods (but we don't enforce it lol, because they can still have their uses) ####
   # private
-  
+
   def load_pad_single(db_name, parent_folder: @parent_folder || PARENT_FOLDER)
     db_linelist = read_file_lines(@database_file_name)
     if db_linelist.include?(db_name)
-      @linelist[db_name] = PAD.new(database_folder_name: db_name, endless_add: @endless_add, has_capacity: @has_capacity, dynamically_allocates: @dynamically_allocates, db_size: @db_size) if !parent_folder
-      @linelist[db_name] = PAD.new(database_folder_name: "#{parent_folder}/#{db_name}", endless_add: @endless_add, has_capacity: @has_capacity, dynamically_allocates: @dynamically_allocates, db_size: @db_size) if parent_folder
+      @linelist[db_name] = PAD.new(traverse_hash: @traverse_hash, database_folder_name: db_name, endless_add: @endless_add, has_capacity: @has_capacity, dynamically_allocates: @dynamically_allocates, db_size: @db_size) if !parent_folder
+      @linelist[db_name] = PAD.new(traverse_hash: @traverse_hash, database_folder_name: "#{parent_folder}/#{db_name}", endless_add: @endless_add, has_capacity: @has_capacity, dynamically_allocates: @dynamically_allocates, db_size: @db_size) if parent_folder
     end
   end
 
@@ -85,12 +96,13 @@ class LineDB
       @linelist.delete(db_name)
     end
   end
+
   # TODO: implement rm_rf for dragonruby on windows and linux, and maybe android
   def delete_pad_single(db_name)
     db_linelist = read_file_lines(@database_file_name)
     if db_linelist.include?(db_name)
       @linelist.delete(db_name)
-      FileUtils.rm_rf(db(db_name).database_folder_name)#{@parent_folder}/#{db_name}")
+      FileUtils.rm_rf(db(db_name).database_folder_name)
     end
   end
 
@@ -98,12 +110,17 @@ class LineDB
     db_linelist = read_file_lines(@database_file_name)
     db_list = {}
     db_linelist.each do |db_name|
-      #puts "new pad"
-      db_list[db_name] = PAD.new(database_folder_name: db_name, endless_add: @endless_add, has_capacity: @has_capacity, dynamically_allocates: @dynamically_allocates, db_size: @db_size) if !parent_folder
-      db_list[db_name] = PAD.new(database_folder_name: "#{parent_folder}/#{db_name}", endless_add: @endless_add, has_capacity: @has_capacity, dynamically_allocates: @dynamically_allocates, db_size: @db_size) if parent_folder
+      db_list[db_name] = PAD.new(traverse_hash: @traverse_hash, database_folder_name: db_name, endless_add: @endless_add, has_capacity: @has_capacity, dynamically_allocates: @dynamically_allocates, db_size: @db_size) if !parent_folder
+      db_list[db_name] = PAD.new(traverse_hash: @traverse_hash, database_folder_name: "#{parent_folder}/#{db_name}", endless_add: @endless_add, has_capacity: @has_capacity, dynamically_allocates: @dynamically_allocates, db_size: @db_size) if parent_folder
     end
     db_list
   end
 end
 
-# a = LineDB.new(parent_folder: "./database/GALLERY_db", database_file_name: "./database/db_list.txt")
+# rubocop:enable Style/StringLiterals
+# rubocop:enable Style/MutableConstant
+# rubocop:enable Layout/LineLength
+# rubocop:enable Style/FrozenStringLiteralComment
+# rubocop:enable Style/GuardClause
+# rubocop:enable Style/NegatedIf
+# rubocop:enable Metrics/ParameterLists

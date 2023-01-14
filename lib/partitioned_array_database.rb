ad9f863e683887786c1cdd3597a576498bacbb00
ArityWolf <eigenfruit@gmail.com>
2023-01-14 13:18:10 -0800

    pad

--- lib/partitioned_array_database.rb
+++ lib/partitioned_array_database.rb
@@ -1,5 +1,5 @@
 require_relative 'file_context_managed_partitioned_array_manager'
-# VERSION v0.0.6 - more aliases
+# VERSION v0.0.6 - more aliases and sync with LineDB
 # VERSION v0.0.5 - alias db pad
 # VERSION v0.0.4 - sync with LineDB
 # VERSION v0.0.3 - cleanup

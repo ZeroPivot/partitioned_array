a6701f068f1895d240b4297620f42b0d21b5ba93
AxleShep <eigenfruit@gmail.com>
2024-05-08 16:01:15 +0000

    get_partition_by_is(id) - untested

--- lib/managed_partitioned_array.rb
+++ lib/managed_partitioned_array.rb
@@ -1,4 +1,4 @@
-# rubocop:disable Layout/LineLength, Metrics/ClassLength, Metrics/AbcSize, Metrics/MethodLength, Metrics/ParameterLists, Style/ExplicitBlockArgument, Style/StringLiterals, Style/NegatedIfElseCondition, Style/FileWrite, Style/RedundantReturn, Style/HashSyntax, Style/IdenticalConditionalBranches, Style/NegatedIf, Style/FrozenStringLiteralComment, Style/BlockComments, Style/MutableConstant, Style/GuardClause, Style/EmptyElse
+# rubocop:disable Layout/LineLength, Metrics/ClassLength, Metrics/AbcSize, Metrics/MethodLength, Metrics/ParameterLists, Style/StringLiterals, Style/NegatedIfElseCondition, Style/FileWrite, Style/RedundantReturn, Style/HashSyntax, Style/IdenticalConditionalBranches, Style/NegatedIf, Style/FrozenStringLiteralComment, Style/BlockComments, Style/MutableConstant, Style/GuardClause, Style/EmptyElse
 require_relative 'partitioned_array'
 
 # VERSION v3.3.4 - 3/11/2024 10:32
@@ -43,7 +43,6 @@ require_relative 'partitioned_array'
     end
 =end
 # VERSION v2.2.1 - bug fixes:   def save_last_entry_to_file! -- added FileUtils mkdir methods to make the last entry directory, since I can't figure out how it was done beforehand; BE SURE TO ADD TO DRAGONRUBY (2/19/2023)
-
 
 # UPDATE: VERSION v2.2.0 - bug fixes
 # * load_archive_no_allocate! and load_archive! now work properly; update in DragonRuby Library Accordingly
@@ -169,6 +168,12 @@ class ManagedPartitionedArray < PartitionedArray
   # full copy but
   def replicate2
     Marshal.load(Marshal.dump(@data_arr))
+  end
+
+  # untested - 5/8/2024 9:00AM PST -07
+  def get_partition_by_id(id)
+    db_index = get(id, hash: true)["db_index"]
+    return get_partition(db_index)
   end
 
   # be sure to add to dragonruby CODE
@@ -325,32 +330,29 @@ class ManagedPartitionedArray < PartitionedArray
   def add_at_last(return_added_element_id: true, save_on_partition_add: true, save_last_entry_to_file: true, &block) # for not adding into {} in a
     raise "No block given" if !block_given?
 
-    if @latest_id==@data_arr.size-1 && @dynamically_allocates && @endless_add # might need @dynamically_allocates
+    if @latest_id == @data_arr.size - 1 && @dynamically_allocates && @endless_add # might need @dynamically_allocates
       @partition_addition_amount.times { add_partition }
       save_everything_to_files! if save_on_partition_add
     end
 
     if @latest_id == 0
-     # potential bug fix
-     block.call(@data_arr[@latest_id]) if block_given?
-     @latest_id += 1
+      # potential bug fix
+      block.call(@data_arr[@latest_id]) if block_given?
+      @latest_id += 1
     else
-     block.call(@data_arr[@latest_id]) if block_given?
-     @latest_id += 1
+      block.call(@data_arr[@latest_id]) if block_given?
+      @latest_id += 1
     end
 
     save_partition_by_id_to_file!(@latest_id) if save_on_partition_add
     save_last_entry_to_file! if save_last_entry_to_file # bug fix 3/11/2024
-    return @latest_id-1 if return_added_element_id #bugfix
+    return @latest_id - 1 if return_added_element_id # bugfix
   end
-
-
-
 
   # runtime complexity: O(n)
   def add(return_added_element_id: true, save_on_partition_add: true, save_last_entry_to_file: true, &block)
     # endless add addition here
-    if @endless_add && @data_arr[@latest_id+1].nil?
+    if @endless_add && @data_arr[@latest_id + 1].nil?
       add_partition
 
       # efficiency implementation (not really tested...)
@@ -534,4 +536,4 @@ class ManagedPartitionedArray < PartitionedArray
   alias load! load_everything_from_files!
 end
 
-# rubocop:enable Layout/LineLength, Metrics/ClassLength, Metrics/AbcSize, Metrics/MethodLength, Metrics/ParameterLists, Style/ExplicitBlockArgument, Style/StringLiterals, Style/NegatedIfElseCondition, Style/FileWrite, Style/RedundantReturn, Style/HashSyntax, Style/IdenticalConditionalBranches, Style/NegatedIf, Style/FrozenStringLiteralComment, Style/BlockComments, Style/MutableConstant, Style/GuardClause, Style/EmptyElse
+# rubocop:enable Layout/LineLength, Metrics/ClassLength, Metrics/AbcSize, Metrics/MethodLength, Metrics/ParameterLists, Style/StringLiterals, Style/NegatedIfElseCondition, Style/FileWrite, Style/RedundantReturn, Style/HashSyntax, Style/IdenticalConditionalBranches, Style/NegatedIf, Style/FrozenStringLiteralComment, Style/BlockComments, Style/MutableConstant, Style/GuardClause, Style/EmptyElse

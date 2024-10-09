e54dd24ecb34edd916405d5036409458dd5bc078
The Field Testers <eigenfruit@gmail.com>
2024-10-09 17:15:55 +0000

    cleanup

--- server_skeleton/logger.rb
+++
@@ -1,5 +0,0 @@
-def log(text)
-  File.open("log.txt", "a") do |f|
-    f.puts "LOG: #{text}"
-  end
-end
--- state_machine_tech/main.rb
+++
@@ -1,7 +0,0 @@
-# State Machine Tech
-STARTING_STATE=0
-UNCASTED=1
-CASTED=2
-
-def sigilX(state)
-end
